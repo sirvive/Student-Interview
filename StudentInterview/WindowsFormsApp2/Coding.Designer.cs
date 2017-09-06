d0f12980e24649cf872afef82b935054094219b9
sirvive <31110199+sirvive@users.noreply.github.com>
2017-09-06 13:14:26 -0400

    Add files via upload

---
+++ StudentInterview/WindowsFormsApp2/Coding.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Coding
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Coding));
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(127, 131);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 151);
+            this.button2.TabIndex = 0;
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("OptimusPrincepsSemiBold", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Cornsilk;
+            this.label1.Location = new System.Drawing.Point(68, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(284, 42);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Click to Begin";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Coding
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(413, 296);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Coding";
+            this.Text = " Student  | Interview";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
--- WindowsFormsApp2/Coding.cs
+++ WindowsFormsApp2/Coding.cs
@@ -23,8 +23,6 @@ namespace WindowsFormsApp2
         public void button2_Click(object sender, EventArgs e)
         {
             StartPosition:
-            int x = 1;
-            if (x >= 1)
 
             StudentName();
             InterviewQ();
@@ -48,12 +46,12 @@ namespace WindowsFormsApp2
         public string StudentName()
         {
             Random random = new Random();
-            string[] Cohort3 = { "Adam", "Brad", "Bryan", "Danil",
+            string[] Cohort3 = { "Adam", "Brad", "Brandon", "Bryan", "Danil",
                 "David", "Dinesh", "Eric", "Oriana",
-                "Jacob B.", "Jake","Tyler", "James P.", "Jared", "Joshua",
-                "Keshav", "Martin", "Michael", "Mykal",
+                "Jacob B.", "Jake", "James", "Jared", "Joshua",
+                "Keshav", "Martin", "Mike", "Mykal",
                 "Omar", "Peter" , "Regina" , "Renata" , "Rob" ,
-                   "Sheldon", "Tae", "Thomas", "Twokoko", "Zach"};
+                   "Sheldon", "Steven", "Tae", "Tom", "Twokoko", "Tyler", "Zach"};
 
             string output = Cohort3[random.Next(Cohort3.Length)];
             MessageBox.Show(output);
@@ -69,9 +67,57 @@ namespace WindowsFormsApp2
         public string InterviewQ()
         {
                 Random random = new Random();
-                string[] Question = { "Tell me about yourself. " , "What are you known for?" , "What is your reputation? ",
-                "What is your personal brand? " , "Why do you do what you do?" , "What are your strength? " ,
-                "What motivates you? ", "How do people benefit by working with you?" };
+                string[] Question = {
+                "Tell me about yourself.",
+                "What are you known for?",
+                "What is your reputation?",
+                "What is your personal brand? ",
+                "What do you want to do?",
+                "What motivates you? ",
+                "How do people benefit by working with you?",
+                "What are your strengths? ",
+                "What are your weaknesses?",
+                "Who is someone you look up to? Why?",
+                "Name someone in your career who really made a difference",
+                "Are you willing to travel?",
+                "How much are you willing to travel?",
+                "What's your availability? When can you start work?",
+                "How long do you expect to work for this company?",
+                "Why do you want to leave your current company?",
+                "Why are you looking for a new job?",
+                "Why are you leaving the military?",
+                "Tell me about a time when you disagreed with your boss. How did you handle it?",
+                "How would you handle a disagreement if your boss was wrong?",
+                "What do you expect from a supervisor?",
+                "If I called your boss right now and asked him what is an area that you could improve on, what would he say?",
+                "What are three positive things your last boss would say about you?",
+                "What negative thing would your last boss say about you",
+                "What was the last book you read for fun?",
+                "Tell me about an accomplishment you are most proud of.",
+                "What's the most difficult decision you've made in the last two years and how did you come to that decision?",
+                "What do you consider your most significant accomplishment?",
+                "Why do you believe you are qualified for this position?",
+                "Have you ever accomplished something you did think you could?",
+                "What do you like or dislike about your current position?",
+                "How do you handle pressure?",
+                "Good employees can take the initiative and get the job done. Can you describe yourself in terms of this statement?",
+                "What is the most difficult aspect about your career?",
+                "How have you ever grown or changed over the past few years?",
+                "What do you consider your most significant challenge?",
+                "What goals have you set recently?",
+                "What do you think of your current/past supervisor",
+                "What jobs have you held and why did you leave?",
+                "What is your management style?",
+                "Why did you choose this field of work?",
+                "How do you spend your spare time?",
+                "What are personal characteristics do you feel are necessary for success in this field?",
+                "Do you prefer working with others or by yourself?",
+                "What kind of boss do you prefer?",
+                "How do you respond to negative feedback?",
+                "How do you handle a conflict?",
+                "How long do you plan to stay with us?"};
+
+
             string output = Question[random.Next(Question.Length)];
             MessageBox.Show(output);
             return Question[random.Next(Question.Length)];
